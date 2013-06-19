9b9d86cbab7a7611a9908b660596a996dcd99b07
Michael Wojcieszek <mw2353@columbia.edu>
2013-06-19 13:02:08 -0400

    Upgraded Training algorithm to build and save the term by document matrix all in one shot. Runs way faster now.

--- src/main/java/edu/columbia/cs/event/qa/task/Training.java
+++ src/main/java/edu/columbia/cs/event/qa/task/Training.java
@@ -2,11 +2,12 @@ package edu.columbia.cs.event.qa.task;
 
 import edu.columbia.cs.event.qa.util.EventQAConfig;
 import edu.columbia.cs.event.qa.util.Preprocessor;
-import org.jblas.DoubleMatrix;
+//import org.jblas.DoubleMatrix;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
+//import java.util.Map.Entry;
 import java.util.Map;
+//import java.util.TreeMap;
 import java.io.*;
 
 /**
@@ -21,172 +22,255 @@ public class Training {
 
     private Preprocessor preprocessor;
 
-    private HashMap<Integer, ArrayList<String>> docIndexMap;
-    private HashMap<String, ArrayList<Integer>> wordSeenInDocMap;
-    private ArrayList<String> terms;
+//    private Map<Integer, ArrayList<String>> docIndexMap;
+//    private Map<String, ArrayList<Integer>> wordSeenInDocMap;
+//    private ArrayList<String> terms;
 
-    private DoubleMatrix TxDMatrix;
-    private int numDoc;
+    HashMap<String, String> termByDocumentCount;
+
+//    private DoubleMatrix TxDMatrix;
+    private int numDocs, numTerms;
 
     public Training () {
         preprocessor = new Preprocessor();
-        docIndexMap = new HashMap<Integer,ArrayList<String>>();
-        wordSeenInDocMap = new HashMap<String, ArrayList<Integer>>();
-        terms = new ArrayList<String>();
-        numDoc = 0;
+        termByDocumentCount = new HashMap<String, String>();
+//
+//        docIndexMap = new HashMap<Integer,ArrayList<String>>();
+//        wordSeenInDocMap = new TreeMap<String, ArrayList<Integer>>();
+//        terms = new ArrayList<String>();
     }
 
-    public void loadTrainingData () throws IOException {
-        loadTrainingData(EventQAConfig.getInstance().getProperty("corpus.file"));
+    public void load () throws IOException {
+        load(EventQAConfig.getInstance().getProperty("corpus.file"));
     }
 
-    public void loadTrainingData (String fileName) throws IOException {
+    public void load (String fileName) throws IOException {
 
         String line; int i = 0;
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         while ((line = reader.readLine()) != null) {
-
             ArrayList<String> tokenList = preprocessor.run(line);
-            if (tokenList.size()>0) {
-                docIndexMap.put(i, tokenList);
+            if (tokenList.size() > 0) {
 
-                for (String word : tokenList) {
-                    if (wordSeenInDocMap.containsKey(word)) {
-                        ArrayList<Integer> existingDocs = wordSeenInDocMap.get(word);
+                HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
 
-                        if (!existingDocs.contains(i)) {
-                            existingDocs.add(i);
-                            wordSeenInDocMap.put(word, existingDocs);
-                        }
-
-                    } else {
-                        ArrayList<Integer> existingDocs = new ArrayList<Integer>();
-                        existingDocs.add(i);
-                        wordSeenInDocMap.put(word, existingDocs);
+                for (String token : tokenList) {
+                    int count = 0;
+                    if (wordCount.containsKey(token)) {
+                        count = wordCount.get(token);
                     }
+                    wordCount.put(token, count+1);
                 }
-            }
-            if (i%100 == 0) { System.out.println("Read "+i+" documents"); } i++;
-            if (i > 100) break;
-        }
 
-        for (Map.Entry<String, ArrayList<Integer>> entry: wordSeenInDocMap.entrySet()) {
-            if (entry.getValue().size() > 5) {
-                terms.add(entry.getKey());
-            }
-        }
+                for (Map.Entry<String, Integer> entry: wordCount.entrySet()) {
 
-        numDoc = i;
-    }
+                    String term = entry.getKey();
+                    String rowCounts = "";
 
-    public void buildTxDMatrix() throws Exception {
-        TxDMatrix = DoubleMatrix.zeros(terms.size(), numDoc);
-        for (int i=0; i<terms.size(); i++) {
-            for (int j : wordSeenInDocMap.get(terms.get(i))) {
-                try {
-                    TxDMatrix.put(i, j, getCount(terms.get(i), docIndexMap.get(j)));
-                } catch(ArrayIndexOutOfBoundsException e) {
-                    System.err.println(i+"-"+j);
+                    if (termByDocumentCount.containsKey(term)) {
+                        rowCounts = termByDocumentCount.get(term);
+                        for (int j=rowCounts.split(",").length; j<i; j++) { rowCounts += ",0"; }
+                        rowCounts += ",";
+                    } else {
+                        for (int j=0; j<i; j++) { rowCounts += "0,"; }
+                    }
+
+                    rowCounts += entry.getValue();
+
+                    if (rowCounts.split(",").length != i+1) {
+                        System.out.println("rowCount.split.length != i+1");
+                    }
+
+                    termByDocumentCount.put(term, rowCounts);
                 }
-            }
-            if (i % 1000 == 0) { System.out.println("Processed "+i+" terms"); }
-        }
-    }
-
-    private double getCount (String vocab, ArrayList<String> terms) {
-        double count = 0.0;
-        for (String t: terms) {
-            if (vocab.equals(t)) {
-                count = count+1.0;
+                if (i%1000 == 0) { System.out.println("Read "+i+" documents"); } i++;
+                i++;
             }
         }
-        return count;
+        numDocs = i;
     }
 
-    public void saveTxDMatrix () throws Exception {
-        saveTxDMatrix(EventQAConfig.getInstance().getProperty("term.doc.file"));
+    public void save () throws IOException {
+        save(EventQAConfig.getInstance().getProperty("vocab.file"), EventQAConfig.getInstance().getProperty("term.doc.file"));
     }
 
-    public void saveTxDMatrix (String fileName) throws Exception {
+    public void save (String termFileName, String termByDocFileName) throws IOException {
 
-        PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+        PrintWriter writer1 = new PrintWriter(new FileWriter(termFileName));
+        PrintWriter writer2 = new PrintWriter(new FileWriter(termByDocFileName));
 
-        for (int i=0; i<terms.size(); i++) {
-            double[] row = new double[numDoc];
-
-            for (int j=0; j<row.length; j++) { row[j] = 0.0; }
-
-            for (int j: wordSeenInDocMap.get(terms.get(i))) {
-                row[j] = getCount(terms.get(i), docIndexMap.get(j));
-            }
-
-            for (int j=0; j<row.length; j++) {
-                if (j == row.length-1) {
-                    writer.print(row[j]);
-                } else {
-                    writer.print(row[j]+",");
-                }
-            }
-
-            if (i%1000 == 0) { System.out.println("Processed "+i+" terms"); }
-            writer.println();
-            writer.flush();
+        int i = 0;
+        for (Map.Entry<String, String> entry: termByDocumentCount.entrySet()) {
+            String rowCounts = entry.getValue();
+            for (int j=rowCounts.split(",").length; j<numDocs; j++) { rowCounts += ",0"; }
+            writer1.println(entry.getKey());
+            writer2.println(rowCounts);
+            if (i%100 == 0) { System.out.println("Processed "+i+" terms"); }
+            i++;
         }
-        writer.close();
+        numTerms = i;
+
+        writer1.flush(); writer1.close();
+        writer2.flush(); writer2.close();
     }
 
-    public void printCorpusStats () throws Exception { printCorpusStats(false); }
-
-    public void printCorpusStats (boolean wordsSeenInDocON) throws IOException {
-
+    public void printCorpusStats () {
         System.out.println("********** Printing Corpus Statistics **********");
-        System.out.println("TxD Map Size: "+wordSeenInDocMap.size());
-
-        if (wordsSeenInDocON) {
-            for (Entry<String, ArrayList<Integer>> entry: wordSeenInDocMap.entrySet()) {
-                StringBuilder integerSet = new StringBuilder("");
-                for (Integer i: entry.getValue()) {
-                    if (integerSet.length() == 0)
-                        integerSet.append(i);
-                    else
-                        integerSet.append(","+i);
-                }
-                System.out.println(entry.getKey()+" ==> "+integerSet);
-            }
-        }
-
-        System.out.println("Total Number of Terms: "+terms.size());
-        System.out.println("Total Number of Docs: "+numDoc);
+        System.out.println("Total # of Terms: "+numTerms);
+        System.out.println("Total # of Docs: "+numDocs);
         System.out.println("************************************************");
     }
 
-    public void saveFreqTerms () throws Exception {
-        saveTxDMatrix(EventQAConfig.getInstance().getProperty("vocab.file"));
-    }
-
-    public void saveFreqTerms (String fileName) throws Exception {
-        PrintWriter writer = new PrintWriter(new FileWriter(fileName));
-        for (String word : terms) {
-            writer.println(word);
-        }
-        writer.flush();
-        writer.close();
-    }
-
-    public DoubleMatrix getTxDMatrix () { return TxDMatrix; }
+//    public void loadTrainingData () throws IOException {
+//        loadTrainingData(EventQAConfig.getInstance().getProperty("corpus.file"));
+//    }
+//
+//    public void loadTrainingData (String fileName) throws IOException {
+//
+//        String line; int i = 0;
+//        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+//        while ((line = reader.readLine()) != null) {
+//
+//            ArrayList<String> tokenList = preprocessor.run(line);
+//            if (tokenList.size()>0) {
+//                docIndexMap.put(i, tokenList);
+//
+//                for (String word : tokenList) {
+//                    if (wordSeenInDocMap.containsKey(word)) {
+//                        ArrayList<Integer> existingDocs = wordSeenInDocMap.get(word);
+//
+//                        if (!existingDocs.contains(i)) {
+//                            existingDocs.add(i);
+//                            wordSeenInDocMap.put(word, existingDocs);
+//                        }
+//
+//                    } else {
+//                        ArrayList<Integer> existingDocs = new ArrayList<Integer>();
+//                        existingDocs.add(i);
+//                        wordSeenInDocMap.put(word, existingDocs);
+//                    }
+//                }
+//            }
+//            if (i%100 == 0) { System.out.println("Read "+i+" documents"); } i++;
+//            if (i > 1000) break;
+//        }
+//
+//        for (Map.Entry<String, ArrayList<Integer>> entry: wordSeenInDocMap.entrySet()) {
+//            if (entry.getValue().size() > 5) {
+//                terms.add(entry.getKey());
+//            }
+//        }
+//
+//        numDoc = i;
+//    }
+//
+//    public void buildTxDMatrix() throws Exception {
+//        TxDMatrix = DoubleMatrix.zeros(terms.size(), numDoc);
+//        for (int i=0; i<terms.size(); i++) {
+//            for (int j : wordSeenInDocMap.get(terms.get(i))) {
+//                try {
+//                    TxDMatrix.put(i, j, getCount(terms.get(i), docIndexMap.get(j)));
+//                } catch(ArrayIndexOutOfBoundsException e) {
+//                    System.err.println(i+"-"+j);
+//                }
+//            }
+//            if (i % 1000 == 0) { System.out.println("Processed "+i+" terms"); }
+//        }
+//    }
+//
+//    private double getCount (String vocab, ArrayList<String> terms) {
+//        double count = 0.0;
+//        for (String t: terms) {
+//            if (vocab.equals(t)) {
+//                count = count+1.0;
+//            }
+//        }
+//        return count;
+//    }
+//
+//    public void saveTxDMatrix () throws Exception {
+//        saveTxDMatrix(EventQAConfig.getInstance().getProperty("term.doc.file"));
+//    }
+//
+//    public void saveTxDMatrix (String fileName) throws Exception {
+//
+//        PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+//
+//        for (int i=0; i<terms.size(); i++) {
+//            double[] row = new double[numDoc];
+//
+//            for (int j=0; j<row.length; j++) { row[j] = 0.0; }
+//
+//            for (int j: wordSeenInDocMap.get(terms.get(i))) {
+//                row[j] = getCount(terms.get(i), docIndexMap.get(j));
+//            }
+//
+//            for (int j=0; j<row.length; j++) {
+//                if (j == row.length-1) {
+//                    writer.print(row[j]);
+//                } else {
+//                    writer.print(row[j]+",");
+//                }
+//            }
+//
+//            if (i%100 == 0) { System.out.println("Processed "+i+" terms"); }
+//            writer.println();
+//            writer.flush();
+//        }
+//        writer.close();
+//    }
+//
+//    public void printCorpusStats () throws Exception { printCorpusStats(false); }
+//
+//    public void printCorpusStats (boolean wordsSeenInDocON) throws IOException {
+//
+//        System.out.println("********** Printing Corpus Statistics **********");
+//        System.out.println("TxD Map Size: "+wordSeenInDocMap.size());
+//
+//        if (wordsSeenInDocON) {
+//            for (Entry<String, ArrayList<Integer>> entry: wordSeenInDocMap.entrySet()) {
+//                StringBuilder integerSet = new StringBuilder("");
+//                for (Integer i: entry.getValue()) {
+//                    if (integerSet.length() == 0)
+//                        integerSet.append(i);
+//                    else
+//                        integerSet.append(","+i);
+//                }
+//                System.out.println(entry.getKey()+" ==> "+integerSet);
+//            }
+//        }
+//
+//        System.out.println("Total Number of Terms: "+terms.size());
+//        System.out.println("Total Number of Docs: "+numDoc);
+//        System.out.println("************************************************");
+//    }
+//
+//    public void saveFreqTerms () throws Exception {
+//        saveTxDMatrix(EventQAConfig.getInstance().getProperty("vocab.file"));
+//    }
+//
+//    public void saveFreqTerms (String fileName) throws Exception {
+//        PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+//        for (String word : terms) {
+//            writer.println(word);
+//        }
+//        writer.flush();
+//        writer.close();
+//    }
+//
+//    public DoubleMatrix getTxDMatrix () { return TxDMatrix; }
 
     public static void main (String[] args) {
         try {
             Training trainer = new Training();
             long a = System.currentTimeMillis();
-            trainer.loadTrainingData();
+            trainer.load();
             long b = System.currentTimeMillis();
-            trainer.buildTxDMatrix();
+            trainer.save();
             long c = System.currentTimeMillis();
-            //trainer.saveFreqTerms();
             trainer.printCorpusStats();
-            System.out.println("Loading time: "+(b-a)+"ms");
-            System.out.println("Building time: "+(c-b)+"ms");
+            System.out.println("Building time: "+(b-a)+"ms");
+            System.out.println("Saving time: "+(c-b)+"ms");
         } catch (IOException e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
